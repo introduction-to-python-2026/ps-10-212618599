6fca50ee38518999c7de6646b886db79e99f29f0
212618599 <noa.zilberberg@live.biu.ac.il>
2026-01-03 19:14:19 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,3 +1,5 @@
+from skimage.morphology import ball
+from skimage.filters import median
 from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
@@ -5,25 +7,27 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 def load_image(path):
- img = Image.open(path)
- img_array = np.array(img)
- if img_array.ndim == 2:
-  img_array = img_array[:, :, np.newaxis]
- return img
-
+    img = Image.open(path)
+    img_array = np.array(img)
+    if img_array.ndim == 2:
+        img_array = img_array[:, :, np.newaxis]
+    return img_array
 
 def edge_detection(image):
- if image.ndim == 3 and image.shape[-1] == 3:
-  gray_image = np.mean(image, axis=2)
- else:
-  gray_image = image
- kernelY = np.array([[ 1, 2, 1],
+    if image.ndim == 3 and image.shape[-1] == 3:
+        gray_image = np.mean(image, axis=2)  # Convert to grayscale
+    else:
+        gray_image = image
+        
+    kernelY = np.array([[ 1, 2, 1],
                         [ 0, 0, 0],
                         [ -1, -2, -1]])
- kernelX = np.array([[ 1, 0, -1],
+    
+    kernelX = np.array([[ 1, 0, -1],
                         [ 2, 0, -2],
                         [ 1, 0, -1]])
- edge_X = signal.convolve2d(gray_image, kernelX, mode='same')
- edge_Y = signal.convolve2d(gray_image, kernelY, mode='same')
- edgeMAG = np.sqrt[(edge_X**2 + edge_Y**2)]
- return edgeMAG
+    edge_X = signal.convolve2d(gray_image, kernelX, mode='same')
+    edge_Y = signal.convolve2d(gray_image, kernelY, mode='same')
+    edgeMAG = np.sqrt(edge_X**2 + edge_Y**2)
+    edgeMAG = np.clip(edgeMAG, 0, 255)
+    return edgeMAG
