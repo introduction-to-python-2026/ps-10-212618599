56bbba09f6961554847a4d5f3ce7c449b263fa3c
212618599 <noa.zilberberg@live.biu.ac.il>
2026-01-03 18:49:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ test=load_image("C:\Users\noazi\Downloads\Road_runner.jpg")
 clean_image = median(test)
 edge_test=edge_detection(clean_image)
 plt.imshow(edge_test, cmap='gray')
-edge_binary = np.logical_and(edge_test > 5 ,edge_test < 7)
+edge_binary = edge > 50
 edge_image = Image.fromarray(edge_binary)
 edge_image.save('my_edges.png')
