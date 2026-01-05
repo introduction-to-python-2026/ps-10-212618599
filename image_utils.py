0b3540daa59f0f28d89586b854dcbcf3926ec91a
212618599 <noa.zilberberg@live.biu.ac.il>
2026-01-05 11:50:17 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -9,16 +9,10 @@ import matplotlib.pyplot as plt
 def load_image(path):
     img = Image.open(path)
     img_array = np.array(img)
-    if img_array.ndim == 2:
-        img_array = img_array[:, :, np.newaxis]
     return img_array
 
 def edge_detection(image):
-    if image.ndim == 3 and image.shape[-1] == 3:
-        gray_image = np.mean(image, axis=2)  # Convert to grayscale
-    else:
-        gray_image = image
-        
+    gray_image = np.mean(image, axis=2)  # Convert to grayscale   
     kernelY = np.array([[ 1, 2, 1],
                         [ 0, 0, 0],
                         [ -1, -2, -1]])
@@ -29,5 +23,4 @@ def edge_detection(image):
     edge_X = signal.convolve2d(gray_image, kernelX, mode='same')
     edge_Y = signal.convolve2d(gray_image, kernelY, mode='same')
     edgeMAG = np.sqrt(edge_X**2 + edge_Y**2)
-    edgeMAG = np.clip(edgeMAG, 0, 255)
     return edgeMAG
