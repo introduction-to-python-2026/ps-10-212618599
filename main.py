98d6d60d25d8dc8795ccd568e123cc9e21b56e77
212618599 <noa.zilberberg@live.biu.ac.il>
2026-01-05 12:35:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import matplotlib as plt
+import numpy as np
 from skimage.filters import median
 from skimage.morphology import ball
 from PIL import Image
-test=load_image("C:\Users\noazi\Downloads\Road_runner.jpg")
+from image_utils import load_image , edge_detection
+
+
+test=load_image("Road_runner.jpg")
 clean_image = median(test,ball(3))
 edge_test=edge_detection(clean_image)
-plt.imshow(edge_test, cmap='gray')
-edge_binary = edge > 50
-edge_image = Image.fromarray(edge_binary)
+threshold_value=50
+edge_binary = edge_test > threshold_value
+
+plt.figure(figsuze=((10,5))
+plt.imshow(edge_binary, cmap='gray')
+plt.axis('off')
+plt.show()
+
+edge_image = Image.fromarray(edge_binary*255)
 edge_image.save('my_edges.png')
